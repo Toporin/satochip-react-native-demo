905e2f0de860b07fcfd7f64c9f78f858403385a7
Toporin <satochip.wallet@gmail.com>
2025-10-07 14:25:44 +0200

    Add satochip-react-native module

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const { getDefaultConfig } = require("metro-config");
 
 module.exports = (async () => {
@@ -6,6 +7,12 @@ module.exports = (async () => {
   } = await getDefaultConfig();
 
   return {
+    // Add watchFolders to watch your local modules
+    watchFolders: [
+      path.resolve(__dirname, '../../goose/satochip-react-native'),
+      path.resolve(__dirname, '../../goose/cktap-protocol-react-native'),
+    ],
+
     transformer: {
       getTransformOptions: async () => ({
         transform: {
@@ -15,12 +22,27 @@ module.exports = (async () => {
       }),
       babelTransformerPath: require.resolve("react-native-svg-transformer"),
     },
+
     resolver: {
       extraNodeModules: {
+        // Add all the polyfills needed
         stream: require.resolve("stream-browserify"),
+        crypto: require.resolve("react-native-crypto"),
+        fs: require.resolve("react-native-level-fs"),
+        path: require.resolve("path-browserify"),
+        process: require.resolve("process/browser"),
+        dgram: require.resolve("react-native-udp"),
+        console: require.resolve("console-browserify"),
       },
       assetExts: assetExts.filter(ext => ext !== "svg"),
       sourceExts: [...sourceExts, "svg"],
+
+      // Add node_modules paths to help resolve modules
+      nodeModulesPaths: [
+        path.resolve(__dirname, 'node_modules'),
+        path.resolve(__dirname, '../../goose/satochip-react-native/node_modules'),
+        path.resolve(__dirname, '../../goose/cktap-protocol-react-native/node_modules'),
+      ],
     },
   };
 })();
